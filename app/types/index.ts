58561912379eb63577de69b50158406a63e61443
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2025-12-01 20:42:57 +0545

    revert back

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { Listing, Reservation, User } from "@/generated/prisma/client";
+=======
+import { Listing, Reservation, User } from "../../generated/prisma/client";
+>>>>>>> d3106684811cb076ae928d15fb6ae800cda9b232
 
 export type SafeUser = Omit<
     User, 'createdAt' | 'updatedAt' | 'emailVerified'
